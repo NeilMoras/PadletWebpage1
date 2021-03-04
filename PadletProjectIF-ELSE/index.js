82c169c9e066822c22910d30318a59aafdb099fd
Neilmoras <neilmoras@gmail.com>
2021-03-03 19:08:56 -0500

    change

---
+++ PadletProjectIF-ELSE/index.js
@@ -0,0 +1,115 @@
+
+
+// // Set the student's grade
+//
+// var grade = prompt("PLease enter your grades", "from 0 to 100");
+//
+// switch (true) {
+//     // If score is 90 or greater
+//     case grade >= 90:
+//         console.log("A");
+//         break;
+//     // If score is 80 or greater
+//     case grade >= 80:
+//         console.log("B");
+//         break;
+//     // If score is 70 or greater
+//     case grade >= 70:
+//         console.log("C");
+//         break;
+//     // If score is 60 or greater
+//     case grade >= 60:
+//         console.log("D");
+//         break;
+//     // Anything 59 or below is failing
+//     default:
+//         console.log("F");
+// }
+
+// if( condition ) {
+//   // run if statement is true
+// }
+// //run some other code
+//
+//
+// if( condition ) {
+//   // statement
+// } else {
+//   // statement (when condition evaluates to false)
+// }
+//
+//
+//
+// if (condition_1) {
+//    // statments
+// } else if (condition_2) {
+//   // statments
+// } else {
+//    // statments
+// }
+//
+//
+//
+//
+// let a = 10,
+//  b = 20;
+// if (a > b) {
+//  console.log('a is greater than b');
+// } else if (a < b) {
+//  console.log('a is less than b');
+// } else {
+//  console.log('a is equal to b');
+// }
+//
+//
+//
+//
+// var i = 10;
+//
+// if (i == 10) {
+//
+//   // First if statement
+//   if (i < 15)
+//     console.log("i is smaller than 15");
+//
+//   // Nested - if statement
+//   // Will only be executed if statement above
+//   // it is true
+//   if (i < 12)
+//     console.log("i is smaller than 12 too");
+//   else
+//     console.log("i is greater than 15");
+// }
+//
+//
+//
+// if (condition1)
+// {
+//    // Executes only when condition1 is true
+//    if (condition2)
+//    {
+//       // Executes when condition2 is true
+//    }
+//    else{
+//      //Executes if condition 1 id not true
+//    }
+// }
+//
+//
+//
+// switch(variable/expression) {
+//     case value1:
+//         // body of case 1
+//         break;
+//
+//     case value2:
+//         // body of case 2
+//         break;
+//
+//     case valueN:
+//         // body of case N
+//         break;
+//
+//     default:
+//         // body of default
+// }
